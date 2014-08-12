221c2615afc0eb8ec289c74126096710c7d7370b
erikvaningen <erikvaningen@gmail.com>
2014-08-12 14:57:44 +0200

    adding asfis to the test data

--- refpub-dto/src/main/java/org/fao/fi/refpub/webservice/test/mock/CodeSystemMock.java
+++ refpub-dto/src/main/java/org/fao/fi/refpub/webservice/test/mock/CodeSystemMock.java
@@ -1,17 +1,69 @@
 package org.fao.fi.refpub.webservice.test.mock;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.fao.fi.refpub.webservice.Code;
 import org.fao.fi.refpub.webservice.CodeSystem;
+import org.fao.fi.refpub.webservice.MultilingualType;
+import org.fao.fi.refpub.webservice.TextType;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 public class CodeSystemMock {
 
+	public static String csvFileName = "src/main/resources/CL_SPECIES_1_3.csv";
 	public final static String CODE = "asfis";
 
+	public static final CodeSystem cl = new CodeSystem();
+
 	public static CodeSystem create() {
-		CodeSystem c = new CodeSystem();
-		c.setCode(CODE);
-		c.setMultilingualName(MultilingualTypeMock.create());
-		c.setResourceUrl(ResourceUrlMock.create("codesystem") + CODE);
-		return c;
+		cl.setCode(CODE);
+		cl.setMultilingualName(MultilingualTypeMock.create());
+		cl.setResourceUrl(ResourceUrlMock.create("codesystem") + CODE);
+		return cl;
 	}
 
+	static {
+		try {
+			Reader r = new InputStreamReader(CodeSystemMock.class.getClassLoader().getResourceAsStream(
+					"CL_SPECIES_1_3.csv"));
+			CSVReader reader = new CSVReader(r, '\t');
+			reader.readNext();
+			String[] nextLine;
+			CodeSystem cl = new CodeSystem();
+			while ((nextLine = reader.readNext()) != null) {
+				Code code = new Code();
+				String codeValue = nextLine[0];
+				if (!StringUtils.isBlank(codeValue)) {
+					code.setCode(codeValue);
+
+					MultilingualType l = new MultilingualType();
+					code.setName(l);
+
+					add2Codes(code.getName().getTexts(), "en", nextLine[4]);
+					add2Codes(code.getName().getTexts(), "fr", nextLine[5]);
+					add2Codes(code.getName().getTexts(), "es", nextLine[6]);
+					add2Codes(code.getName().getTexts(), "la", nextLine[7]);
+					add2Codes(code.getName().getTexts(), "fr", nextLine[8]);
+					cl.getCodeLists().add(code);
+				}
+			}
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void add2Codes(List<TextType> names, String lang, String v) {
+		if (!StringUtils.isBlank(v)) {
+			TextType t = new TextType();
+			t.setLang(lang);
+			t.setText(v);
+			names.add(t);
+		}
+	}
 }
---
+++ refpub-dto/src/test/java/org/fao/fi/refpub/webservice/test/mock/CodeSystemMockTest.java
@@ -0,0 +1,12 @@
+package org.fao.fi.refpub.webservice.test.mock;
+
+import org.junit.Test;
+
+public class CodeSystemMockTest {
+
+	@Test
+	public void testCreate() {
+		CodeSystemMock.create();
+	}
+
+}
