f603089ebf47cc2b2f2b70a2a2e0772e654cb8ba
erikvaningen <erikvaningen@gmail.com>
2015-01-15 17:38:32 +0100

    upgraded cdi-unit. Started with unit tests of the CoreWS, not finished yet.

---
+++ refpub-web/src/test/java/org/refpub/webservice/core/CoreWsTest.java
@@ -0,0 +1,100 @@
+package org.refpub.webservice.core;
+
+import javax.inject.Inject;
+
+import org.fao.fi.refpub.webservice.beans.RefPubImplementation;
+import org.jglue.cdiunit.AdditionalClasses;
+import org.jglue.cdiunit.CdiRunner;
+import org.jglue.cdiunit.InRequestScope;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CdiRunner.class)
+// @ActivatedAlternatives(Producer.class)
+@AdditionalClasses({ RefPubImplementation.class })
+@RefPubSupportJaxRs
+@InRequestScope
+public class CoreWsTest {
+
+	@Inject
+	CoreWs coreWs;
+
+	// @Inject
+	// ContextController contextController;
+
+	String type = "json";
+	String conceptCode = "Species";
+	String codesystem = "ASFIS";
+	String code = "TUN";
+	String attribute = "lang";
+	String group = "Family";
+	String subgroup = "Order";
+
+	@Test
+	@Ignore
+	public void testConcepts() {
+		coreWs.concepts(type);
+	}
+
+	@Test
+	@Ignore
+	public void testCodeString() {
+		coreWs.code(type);
+	}
+
+	@Test
+	@Ignore
+	public void testConceptList() {
+		coreWs.conceptList(conceptCode, type);
+	}
+
+	@Test
+	@Ignore
+	public void testAttributeList() {
+		coreWs.attributeList(conceptCode, type);
+	}
+
+	@Test
+	@Ignore
+	public void testCodesystems() {
+		coreWs.codesystems(conceptCode, type);
+	}
+
+	@Test
+	@Ignore
+	public void testCodesystem() {
+		coreWs.codesystem(conceptCode, codesystem, type);
+	}
+
+	@Test
+	@Ignore
+	public void testCodeStringStringStringString() {
+		coreWs.code(conceptCode, codesystem, code, type);
+	}
+
+	@Test
+	@Ignore
+	public void testAttrCodeXML() {
+		coreWs.attrCodeXML(conceptCode, codesystem, codesystem, attribute, type);
+	}
+
+	@Test
+	@Ignore
+	public void testGetGroup() {
+		coreWs.getGroup(conceptCode, type);
+	}
+
+	@Test
+	@Ignore
+	public void testGroupMain() {
+		coreWs.groupMain(conceptCode, group, type);
+	}
+
+	@Test
+	@Ignore
+	public void testGetGroupXML() {
+		coreWs.getGroupXML(conceptCode, group, subgroup, type);
+	}
+
+}
---
+++ refpub-web/src/test/java/org/refpub/webservice/core/RefPubMockHttpServletRequestImpl.java
@@ -0,0 +1,7 @@
+package org.refpub.webservice.core;
+
+import org.jglue.cdiunit.internal.servlet.MockHttpServletRequestImpl;
+
+public class RefPubMockHttpServletRequestImpl extends MockHttpServletRequestImpl {
+
+}
