94c2d1f5a2cc1c6af2b89d2ffdf727c80c206f71
Enrico Anello <enrico.anello@fao.org>
2015-02-17 13:21:53 +0100

    ...

--- refpub-web/src/main/java/org/refpub/webapp/RefPubBeanRegistration.java
+++ refpub-web/src/main/java/org/refpub/webapp/RefPubBeanRegistration.java
@@ -1,14 +1,18 @@
 package org.refpub.webapp;
 
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
 import org.fao.fi.refpub.webservice.beans.RefPubImplementation;
 import org.fao.fi.refpub.webservice.beans.RefPubInterface;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 public class RefPubBeanRegistration extends AbstractBinder{
+	
+	@Context ServletContext context;
 
 	@Override
 	protected void configure() {
 		bind(RefPubImplementation.class).to(RefPubInterface.class);
 	}
-
 }
